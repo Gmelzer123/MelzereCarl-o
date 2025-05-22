d674b01011ae80f216201c37cf114051c117f64f
Gmelzer123 <142441863+Gmelzer123@users.noreply.github.com>
2025-05-22 08:50:27 -0300

    Add files via upload

---
+++ front end portifolio/exercício1/script.js
@@ -0,0 +1,20 @@
+let num1
+let num2
+let resultado
+let parouimpar
+
+num1 = Number(window.prompt("digite um numero"));
+num2 = Number (window.prompt("digite outro numero"));
+
+resultado = num1 + num2
+
+parouimpar = resultado % 2
+
+if(parouimpar == 0){
+    alert("o número" + resultado + " é par")
+}else {
+    alert("o numero" + resultado + "é impar")
+}
+
+alert(resultado)
+
