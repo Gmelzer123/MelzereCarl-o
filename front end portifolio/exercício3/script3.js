0872da0d0277b46a4e669fd60e9d86b59f0a1ded
Gmelzer123 <142441863+Gmelzer123@users.noreply.github.com>
2025-05-27 08:36:07 -0300

    Add files via upload

---
+++ front end portifolio/exercício3/script3.js
@@ -0,0 +1,17 @@
+function media(){
+    let num1 = Number(window.prompt("digite aqui o primeiro número:"))
+    let num2 = Number (window.prompt("digite o segundo número:"))
+    let num3 = Number (window.prompt("digite um terceiro número"))
+    
+    let resultado = num1 + num2 + num3;
+
+    alert("O resultado é" + resultado + ".");
+}
+
+function resultado{
+    let resultado = Number (window.prompt("digite aqui o resultado"))
+
+    let media = resultado / 3
+}
+     
+
