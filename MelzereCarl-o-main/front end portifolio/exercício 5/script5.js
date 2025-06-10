b57ba35abc8b2ce3d5b6eb6b1c30fade7d4edd00
Gmelzer123 <142441863+Gmelzer123@users.noreply.github.com>
2025-06-10 08:51:53 -0300

    Add files via upload

---
+++ MelzereCarl-o-main/front end portifolio/exercício 5/script5.js
@@ -0,0 +1,9 @@
+function acesse(){
+    let num1 = Number(window.prompt("digite aqui o primeiro número:"))
+    let num2 = Number (window.prompt("digite o segundo número:"))
+}
+
+if(acesse())
+   
+
+
