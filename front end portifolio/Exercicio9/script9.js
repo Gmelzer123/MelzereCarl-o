59ec48944d8715e5d9eeb2b2e5779f15c56ccf8c
Gmelzer123 <142441863+Gmelzer123@users.noreply.github.com>
2025-06-26 10:42:47 -0300

    Add files via upload

---
+++ front end portifolio/Exercicio9/script9.js
@@ -0,0 +1,13 @@
+let mostraHTML = document.getElementById("mostraHTML")
+let text
+let OUTONO = ("JUNHO, MARCO")
+let VERAO 
+let INVERNO
+let PRIMAVERA
+
+function teste() {
+    let text = prompt("Digite um texto").toUpperCase();
+    
+    console.log(text);
+}
+
---
+++ front end portifolio/exercicio4/script4.js
@@ -0,0 +1,2 @@
+let num1 = Number(window.prompt("digite aqui o primeira equipe:"))
+let num2 = Number (window.prompt("digite o segundo número:"))
