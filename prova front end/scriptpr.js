34f3da1234ce7a132dfdc972a96875cff7d22ca8
Gmelzer123 <142441863+Gmelzer123@users.noreply.github.com>
2025-07-03 10:51:49 -0300

    Add files via upload

---
+++ prova front end/scriptpr.js
@@ -0,0 +1,26 @@
+let mostraHTML = document.getElementById("mostraHTML");
+
+    let time1
+    let time2
+    let time3
+    let time4
+
+    let nome1
+    let nome2
+    let nome3
+
+function  cadastro(){
+    time1 = (window.prompt("digite o nome do time")).toUpperCase();
+    mostraHTML.innerHTML += `<p>${time1} e seus integrantes tem a media de: ${media_idade}</p>`
+
+    nome1 = (window.prompt("digite o nome do jogador 1:"));
+    
+    nome2 = (window.prompt("digite o nome do jogador 2"));
+
+    nome3 = (window.prompt("digite o nome do jogador 3"));
+
+    var 
+
+}
+    
+
